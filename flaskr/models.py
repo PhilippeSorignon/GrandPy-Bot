50d992e0f5afe2a0ad6ed7ab7741a3a6c562bade
Philippe Sorignon <philippe.sorignon@protonmail.com>
2020-02-15 15:16:16 +0100

    The API calls are now managed by a class

--- flaskr/models.py
+++ flaskr/models.py
@@ -3,46 +3,52 @@ import requests
 import re
 from .settings import API_KEY
 
+'''Make the APIs calls and return formatted data'''
+class Api:
+    '''Init constructor'''
+    def __init__(self):
+        self.user_request = ''
+        self.full_adress = ''
+        self.wikipedia_description = ''
 
-def parse_adress(user_input):
-    with open('flaskr/static/fr.txt') as json_file:
-        stop_words = json.load(json_file)
-    phrase = ""
-    word = ""
-    for i in range(len(user_input)):
-        if user_input[i] != " " and user_input[i] != "'":
-            word += (user_input[i])
-        else:
-            if word in stop_words:
-                phrase = ""
+    '''Return the place to search from the user input'''
+    def parse_adress(self, user_input):
+        with open('flaskr/static/fr.txt') as json_file:
+            stop_words = json.load(json_file)
+        word = ""
+        for i in range(len(user_input)):
+            if user_input[i] != " " and user_input[i] != "'":
+                word += (user_input[i])
             else:
-                phrase += word + " "
+                if word in stop_words:
+                    self.user_request = ""
+                else:
+                    self.user_request += word + " "
 
-            word = ""
-    phrase += re.sub('[ !?.]', '', word)
-    phrase = re.sub(' ', '%20', phrase)
+                word = ""
+        self.user_request += re.sub('[ !?.]', '', word)
 
-    if phrase[len(phrase) - 1] == ' ':
-        phrase = phrase[:len(phrase) - 1]
-
-    return phrase
-
-
-def find_full_adress(user_adress):
-    request_data = {'query':user_adress, 'key':API_KEY}
-
-    r = requests.get('https://maps.googleapis.com/maps/api/place/textsearch/json?',\
-     params=request_data).json()['results']
-
-    return r[0]['formatted_address']
+        if self.user_request[len(self.user_request) - 1] == ' ':
+            self.user_request = self.user_request[:len(self.user_request) - 1]
 
 
-def get_wikipedia(user_input):
-    # format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Paris
-    request_data = {'format':'json', 'action':'query', 'prop':'extracts', 'exintro':'', 'explaintext':'', 'redirects':'1', 'titles':user_input}
+    '''Return the exact adress using the Google Places API'''
+    def find_full_adress(self, user_adress):
+        request_data = {'query':user_adress, 'key':API_KEY}
 
-    r = requests.get('https://fr.wikipedia.org/w/api.php?', params=request_data).json()['query']
+        r = requests.get('https://maps.googleapis.com/maps/api/place/textsearch/json?',\
+         params=request_data).json()['results']
 
-    phrase = re.sub('[\[\]]', '', r['pages'][list(r['pages'].keys())[0]]['extract'])
-    phrase = re.sub(' ', '%20', phrase)
-    return phrase
+        self.full_adress = r[0]['formatted_address']
+
+
+    '''Return the Wikipedia introduction'''
+    def get_wikipedia(self):
+        # format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Paris
+        request_data = {'format':'json', 'action':'query', 'prop':'extracts',
+                        'exintro':'', 'explaintext':'', 'redirects':'1', 'titles':self.user_request}
+
+        r = requests.get('https://fr.wikipedia.org/w/api.php?', params=request_data).json()['query']
+
+        self.wikipedia_description = re.sub('[\[\]]', '',
+                                            r['pages'][list(r['pages'].keys())[0]]['extract'])
--- flaskr/views.py
+++ flaskr/views.py
@@ -1,31 +1,32 @@
 from flask import Blueprint, render_template, request
 import json, re
-from .models import parse_adress, find_full_adress, get_wikipedia
+from .models import Api
 from .settings import API_KEY
 
 global user_place
 
 views = Blueprint("views", __name__, static_folder="static", template_folder="templates")
 
+api_instance = Api()
+
 @views.route('/')
 def home():
     return render_template("base.html")
 
 # Passer en POST
-@views.route('/map/', methods=['GET'])
+@views.route('/map')
 def map():
-    data = request.args.to_dict()
+    data = {}
+    data['adress'] = api_instance.full_adress
+    data['wiki'] = api_instance.wikipedia_description
     return render_template("maps.html", data=data, api_key=API_KEY)
 
 
 @views.route('/api', methods=['POST'])
 def api():
-    user_request = request.form['user_data']
-    user_request = parse_adress(user_request)
-    full_adress = user_request #find_full_adress(user_request)
-    user_request = re.sub('[%20]', ' ', user_request)
-    wikipedia = get_wikipedia(user_request)
-    r = {}
-    r['adress'] = full_adress
-    r['wiki'] = wikipedia
-    return r
+    api_instance.parse_adress(request.form['user_data'])
+    #full_adress = user_request #find_full_adress(user_request)
+    api_instance.full_adress = api_instance.user_request
+    #user_request = re.sub('[%20]', ' ', user_request)
+    api_instance.get_wikipedia()
+    return 'ok'
