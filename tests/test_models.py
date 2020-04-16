5b01cfc47f43da25ef5d784fa3dd2507bea2ef26
Philippe Sorignon <philippe.sorignon@protonmail.com>
2020-04-16 13:18:47 +0200

    Mock

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,10 @@
 from flaskr.models import Api
 
+import urllib.request
+
+from io import BytesIO
+import json
+
 class TestView:
     test = Api()
     def test_parse_view(self):
@@ -10,7 +15,16 @@ class TestView:
         self.test.parse_adress("Bonjour, je cherche où se trouve la Tour Eiffel.")
         assert self.test.user_request == "Tour Eiffel"
 
+    def test_http_return(self, monkeypatch):
+        self.test.parse_adress("Bonjour, je cherche où se trouve la Tour Eiffel.")
 
-'''def test_return_full_adress():
-    \'''Test if the call to the google place API return what we expect\'''
-    assert find_full_adress("OpenClassrooms") == "7 Cité Paradis, 75010 Paris, France"'''
+        r = [{'place_id': 80582518, 'licence': 'Data © OpenStreetMap contributors, ODbL 1.0. https://osm.org/copyright', 'osm_type': 'way', 'osm_id': 5013364, 'boundingbox': ['48.8574753', '48.8590465', '2.2933084', '2.2956897'], 'lat': '48.858260200000004', 'lon': '2.2944990543196795', 'display_name': 'Tour Eiffel, 5, Avenue Anatole France, Quartier du Gros-Caillou, Paris, Île-de-France, France métropolitaine, 75007, France', 'class': 'tourism', 'type': 'attraction', 'importance': 0.7868325701744197, 'icon': 'https://nominatim.openstreetmap.org/images/mapicons/poi_point_of_interest.p.20.png'}]
+
+        def mockreturn(request):
+            return BytesIO(json.dumps(r).encode())
+
+        monkeypatch.setattr(urllib.request, 'urlopen', mockreturn)
+
+        self.test.find_full_adress()
+
+        assert self.test.full_adress == "Tour Eiffel, 5, Avenue Anatole France, Quartier du Gros-Caillou, Paris, Île-de-France, France métropolitaine, 75007, France"
