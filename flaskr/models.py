f29b9eb15b1244d270750fcca1b029b3356c9147
Philippe Sorignon <philippe.sorignon@protonmail.com>
2020-05-12 16:20:53 +0200

    Fixed what should be the last issue

--- flaskr/models.py
+++ flaskr/models.py
@@ -30,7 +30,7 @@ class Api:
         if self.user_request[len(self.user_request) - 1] == ' ':
             self.user_request = self.user_request[:len(self.user_request) - 1]
 
-        self.user_request = re.sub(' ', '%20', self.user_request.title())
+        self.user_request = self.user_request.title()
 
 
     def find_full_adress(self):
@@ -40,11 +40,9 @@ class Api:
         r = requests.get('https://nominatim.openstreetmap.org/search?',\
          params=request_data).json()
         if r:
-            self.full_adress = re.sub(' ', '%20', r[0]['display_name'])
+            self.full_adress = r[0]['display_name']
         else:
             self.full_adress = 'NotFound'
-
-
 
 
     def get_wikipedia(self):
@@ -60,4 +58,7 @@ class Api:
         else:
             self.wikipedia_description = re.sub('[\[\]]', '',
                                                 r['pages'][list(r['pages'].keys())[0]]['extract'])
-            self.wikipedia_description = re.sub(' ', '%20', self.wikipedia_description)
+
+        self.user_request = re.sub(' ', '%20', self.user_request)
+        self.full_adress = re.sub(' ', '%20', self.full_adress)
+        self.wikipedia_description = re.sub(' ', '%20', self.wikipedia_description)
